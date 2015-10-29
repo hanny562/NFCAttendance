88f0cb7e98a46fae701aa9c941a308f96b7998f8
hanny562 <hanny562@hotmail.com>
2015-10-30 00:59:40 +0800

    Add about me class and edit activity_about

---
+++ app/src/main/java/cap2100/nfcattendance/About.java
@@ -0,0 +1,35 @@
+package cap2100.nfcattendance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class About extends AppCompatActivity {
+
+    WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        mWebView = (WebView) findViewById(R.id.webView);
+
+        String text = "<html><body>"
+                + "<h4>"
+                + "About Me"
+                + "</h4><p align=\"justify\">"
+                + "We are a Team of three of final year students from Inti International College Penang that currently developing a prototype" +
+                "Android Application that illutrate the real scenarios of attendance taking by lecturer in class with more efficient way."
+                + "<br><br>"
+                + "Team Members <br>"
+                + "- Liu Guan Han <br>"
+                + "- Tiffany Toh Qing Wei <br>"
+                + "- Yew Pui Cheng <br><br>"
+                + "Supervisor <br>"
+                + "Mr. Shahriman <br><br>"
+                + "</p></body></html>";
+
+        mWebView.loadData(text, "text/html", "utf-8");
+    }
+}
